730f584c0504d2466e9f5e005f6eeef5773fbcb0
wambui-nyambura-joyce <nyamburawambuui@gmail.com>
2023-03-08 23:38:52 +0300

    rest of questions

--- strings.js
+++ strings.js
@@ -10,3 +10,42 @@ const food = "I was feeling hungry today"
 let verb = "eat"
 console.log(`I was ${verb} feeling hungry today`);
 
+// Count how many times the following string appears in the string variable:
+// 1. "the"
+// 2."brown"
+const story= "The quick brown fox jumps over the lazy dog"
+console.log(story.split("the").length - 1);
+console.log(story.split("brown").length - 1);
+
+// Using JavaScript, find the following words from the following strings:
+// 1. "now"
+const string1 = "We are now going to school";
+console.log(string1.indexOf("now"))
+
+// 2. "sitting"
+const string2 = "The child was sitting on the table before it fell"
+console.log(string2.indexOf("sitting"))
+
+// Convert the following strings into the specified format:
+// 1. UpperCase: "wonderful"
+let affirm = "Wonderful";
+let results = affirm.toUpperCase();
+console.log(results)
+// 2. LowerCase: "amazing", "BEneath"
+let compliment = "amazing";
+let compliments = "BEneath";
+console.log(compliment.toLowerCase(), compliments.toLowerCase());
+
+// 3. Title case "A beautiful wedding"
+
+
+function titleCase(string) {
+    var statement = string.toLowerCase().split(" ");
+    for(var i = 0; i< statement.length; i++){
+       statement[i] = statement[i][0].toUpperCase() + statement[i].slice(1);
+    }
+ document.write(statement.join(" "));
+ return statement;
+ }
+ titleCase("A beautiful Wedding");
+
