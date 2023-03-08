43d67a6719172e71cea9d386d8e0636f979596ab
wambui-nyambura-joyce <nyamburawambuui@gmail.com>
2023-03-08 22:15:12 +0300

    extract characters and inserting strings in a variable

---
+++ strings.js
@@ -0,0 +1,12 @@
+// Extract the last four characters from the string below;
+// "beautiful"
+let text = "beautiful";
+let result = text.substr(5, 8);
+console.log(result);
+
+// insert the following string at the fourth index of the below variable:
+// "eat"
+const food = "I was feeling hungry today"
+let verb = "eat"
+console.log(`I was ${verb} feeling hungry today`);
+
